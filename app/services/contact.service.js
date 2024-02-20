3ce5aca608a148bb6dbc21a0a1b21212c1926cae
W3ik4ng <khangb2014749@student.ctu.edu.vn>
2024-02-20 21:25:20 +0700

    Cai dat cac handler truy suat CSDL

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -29,6 +29,52 @@ class ContactService {
         );
         return result;
     }
+
+    async find(filter) {
+        const cursor = await this.contact.find(filter);
+        return await cursor.toArray();
+    }
+
+    async findByName(name) {
+        return await this.find({
+            name: { $regex: new RegExp(name), $options: "i" },
+        });
+    }
+
+    async findById(id) {
+        return await this.contact.findOne({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+    }
+
+    async update(id, payload) {
+        const filter = {
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        };
+        const update =this.extractContactData(payload);
+        const result = await this.contact.findOneAndUpdate(
+            filter,
+            { $set: update },
+            { returnDocument: "after" }
+        );
+        return result;
+    }
+
+    async delete(id) {
+        const result = await this.contact.findOneAndDelete({
+            _id: ObjectId.isValid(id) ? new ObjectId(id): null,
+        });
+        return result;
+    }
+
+    async findFavorite() {
+        return await this.find({ favorite: true });
+    }
+
+    async deleteAll() {
+        const result = await this.contact.deleteMany({});
+        return result.deletedCount;
+    }
 }
 
 module.exports = ContactService;
